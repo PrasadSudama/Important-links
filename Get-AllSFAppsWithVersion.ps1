9b2c4d528cf3d4a2ae9da8f90ec0cb7ac7c6f9c4
Prasad, Sudama <sudama.prasad@accenture.com>
2018-06-14 05:09:22 +0000

    Added some more links

---
+++ Get-AllSFAppsWithVersion.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module $PSScriptRoot\SelectEnvironment.psm1
+
+Select-Environment
+
+$ApplicationListPath = Read-Host -Prompt 'Enter path where you want to copy the list of applications with version'
+$ApplicationListPath = $ApplicationListPath +'\ApplicationList.csv'
+
+if ($localOrRemote -eq "l")
+{
+    Connect-ServiceFabricCluster `
+        -ConnectionEndpoint $connectionEndpoint
+}
+else
+{
+    Connect-ServiceFabricCluster `
+    -ConnectionEndpoint $connectionEndpoint `
+    -X509Credential `
+    -StoreLocation 'CurrentUser' `
+    -ServerCommonName $ServerCommonName `
+    -FindType 'FindByThumbprint'   `
+    -FindValue $Thumbprint 
+}
+
+ Get-ServiceFabricApplication | Select-Object ApplicationName,ApplicationTypeVersion | Export-Csv $ApplicationListPath -NoTypeInformation
